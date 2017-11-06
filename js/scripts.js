568b0bacb07698433ea38d01567198119ba69ef8
Andrea Gonzales <andreafgonzales@gmail.com>
2017-11-05 20:58:59 -0500

    added different icons

--- js/scripts.js
+++ js/scripts.js
@@ -148,6 +148,11 @@ function initMap() {
 
 //ICONS
 var icons = {
+  animal: {
+    icon: 'images/animal.png',
+    coords: [1, 1, 1, 110, 75, 110, 75, 1],
+    type: 'poly'
+  },
   hangwoman: {
     icon: 'images/hangwoman-short.png',
     coords: [1, 1, 1, 100, 87, 100, 87, 1],
@@ -156,6 +161,31 @@ var icons = {
   cat: {
     icon: 'images/cat.png',
     coords: [1, 1, 1, 75, 82, 75, 82, 1],
+    type: 'poly'
+  },
+  cat1: {
+    icon: 'images/cat1.png',
+    coords: [1, 1, 1, 70, 100, 70, 100, 1],
+    type: 'poly'
+  },
+  cat3: {
+    icon: 'images/cat3.png',
+    coords: [1, 1, 1, 87, 50, 87, 50, 1],
+    type: 'poly'
+  },
+  vtCat: {
+    icon: 'images/vt-cat.png',
+    coords: [1, 1, 1, 70, 100, 70, 100, 1],
+    type: 'poly'
+  },
+  witch: {
+    icon: 'images/witch.png',
+    coords: [1, 1, 1, 88, 100, 88, 100, 1],
+    type: 'poly'
+  },
+  witch1: {
+    icon: 'images/witch-1.png',
+    coords: [1, 1, 1, 64, 100, 64, 100, 1],
     type: 'poly'
   }
 };
@@ -178,12 +208,12 @@ var coordinates = [
             '</div>'+
             '<h1 class="first-heading">Alternative für Deutschland</h1>'+
             '<h4 class="second-heading">Berlin, Germany</h4>'+
-            '<div class="img-align"><img id="afd" src="images/afd.png"/></div>'+
+            // '<div class="img-align"><img id="afd" src="images/afd.png"/></div>'+
             '<div class="body-content">'+
             '<p>Germany held its 2017 Bundestag elections this September. AfD, a far-right Populist party, won 12.6% of the votes, meaning that they will have representatives in Germany’s Parliament. Their campaign posters focused on a Germany first platform, particularly targeting refugees and Muslims. This ad reads ““Burkas?” We wear Bikinis.” This poster caused outrage in Germany for its sexist and Islamophobic messages. However, AfD now has seats in the Bundestag. This is the first time a far-right party has had seats in the German Parliament since the election of the Nazi party.</p>'+
             '</div>'+
             '</div>'],
-  ['Speyer', 49.316830, 8.439153, 5, 'hangwoman',
+  ['Speyer', 49.316830, 8.439153, 5, 'vtCat',
             '<div class="content">'+
             '<div class="site-notice">'+
             '</div>'+
@@ -193,18 +223,18 @@ var coordinates = [
             '<p>Written in 1486, Malleus Maleficarum is known as the foremost treatise on witchcraft. The work was written by clergyman Heinrich Kramer, and later Jacob Sprenger’s name was also added as an author. The book details how people, usually women, are possessed by the devil to become witches. It argues that witchcraft should be tried as a crime in secular courts, and that torture should be used to urge witches to confess. Particularly, the book advocated for witches to be burned at the stake. It remained a bestselling book for nearly 200 years after its publication.</p>'+
             '</div>'+
             '</div>'],
-  ['Rome', 41.902671, 12.496252, 2, 'cat',
+  ['Rome', 41.902671, 12.496252, 2, 'witch',
             '<div class="content">'+
             '<div class="site-notice">'+
             '</div>'+
             '<h1 class="first-heading">Italy\'s Fertility Day</h1>'+
             '<h4 class="second-heading">Rome, Italy</h4>'+
-            '<div class="img-align"><img id="fertility" src="images/fertility.png"/></div>'+
+            // '<div class="img-align"><img id="fertility" src="images/fertility.png"/></div>'+
             '<div class="body-content">'+
             '<p>In 2016, Italy’s Ministry of Health launched a “Fertility Day” campaign that aimed to reverse Italy’s low birthrate. The ads featured sexist slogans, such as one that read, “Beauty doesn’t have an age. Fertility does.” The signs were met with anger over the belittling of women’s issues such as equal workplace pay and affordable childcare. Many people said the ads were reminiscent of Fascist Italy, where women were encouraged to have as many children as possible, because it was seen as their duty to their state.</p>'+
             '</div>'+
             '</div>'],
-  ['Lancashire', 53.850536, -2.668760, 1, 'hangwoman',
+  ['Lancashire', 53.850536, -2.668760, 1, 'witch1',
             '<div class="content">'+
             '<div class="site-notice">'+
             '</div>'+
@@ -214,7 +244,7 @@ var coordinates = [
             '<p>In 1612, twelve people, ten women and two men, were hung in Lancashire, England after being convicted of witchcraft. These accused mainly came from two families: the Demdikes and the Chattoxes. Each family accused the other of witchcraft, potentially due to a underlying fight about property. The matriarch of the Demdikes was Elizabeth Southerns, who was nearly 80 years old when she was hung. She had been accused of being a witch before, due to her old age and possession of pins, which were thought to have healing powers. Southerns was hung along with her daughter and two grandchildren.</p>'+
             '</div>'+
             '</div>'],
-  ['London', 51.507356, -0.127764, 1, 'hangwoman',
+  ['London', 51.507356, -0.127764, 1, 'animal',
             '<div class="content">'+
             '<div class="site-notice">'+
             '</div>'+
@@ -224,7 +254,7 @@ var coordinates = [
             '<p>Nicola Thorp, a British receptionist, was told that her flat shoes were unacceptable for her workplace. Her supervisor explained that women at the PwC accounting firm were required to wear heels that were at least two inches high. Upon investigation, Thorp learned that her firm, and several other companies in England, had dress codes that made women wear heels. After starting a petition to draw attention to the sexist requirement, other women came forward saying that they had been asked to change their appearance in the work places, including dying their hair blonde and wearing more makeup. Thorp’s petition was signed more than 150,000 times.</p>'+
             '</div>'+
             '</div>'],
-  ['Valais', 46.163517, 7.517275, 1, 'hangwoman',
+  ['Valais', 46.163517, 7.517275, 1, 'cat1',
             '<div class="content">'+
             '<div class="site-notice">'+
             '</div>'+
@@ -234,7 +264,7 @@ var coordinates = [
             '<p>These 1428 trials were the first ones in Switzerland that led to systematic execution. Townspeople started being accused of witchcraft in the lower region of Valais, but the accusations quickly began to spread throughout the entirety of Valais. Local authorities proclaimed that trials would begin to convict the witches. Torture was employed if a person was accused by five or more people. The accused witches were charged with murder, heresy and sorcery, and making deals with the devil. The exact number of people executed is unknown, but is estimated to be above 200 people.</p>'+
             '</div>'+
             '</div>'],
-  ['Paris', 48.857482, 2.349461, 1, 'cat',
+  ['Paris', 48.857482, 2.349461, 1, 'cat3',
             '<div class="content">'+
             '<div class="site-notice">'+
             '</div>'+
